412dd037edf57ece1c0b124fd2f52d84a8e88d54
tsurutan <ishaninaritaina623@gmail.com>
2014-11-02 09:28:09 +0900

    マイページの作成

--- my_page.php
+++ my_page.php
@@ -1,8 +1,18 @@
 <html>
+<link rel="stylesheet" href="css/jquery.circliful.css">
 <head>
 <style>
 </style>
+<script src="http://code.jquery.com/jquery-2.1.1.js"></script>
 </head>
 <body>
+<script src="jquery.min.js"></script>
+<script src="jquery.circliful.min.js"></script>
+<script>
+$(document).ready(function(){
+    $('.myStat').circliful();
+});
+</script>
+<div class="myStat"></div>
 </body>
 </html>
