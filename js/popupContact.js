ab98c11f23c1e64536764614efd8287d6da41673
dragogonone <user@YataMac.local>
2014-11-03 13:34:32 +0900

    微調整

--- js/popupContact.js
+++ js/popupContact.js
@@ -4,9 +4,18 @@ function insertClick() {
 	var stpop = new Date($('#stpop').val());//startdate
 	var edpop = new Date($('#edpop').val());//enddate
 	var msgpop = $('#msgpop').val();//description
-	var stiso = stpop.toISOString();
-	var ediso = edpop.toISOString();
-	insertEvent(summary,stpop,edpop,msgpop);//googleCalendarAPI.js
+	
+	var jisa = stpop.getTime() - (9*1000*60*60);
+	var stpop2 = new Date();
+	stpop2.setTime(jisa);
+		
+	jisa = stpop.getTime() - (9*1000*60*60);
+	var edpop2 = new Date();
+	edpop2.setTime(jisa);
+	
+	var stiso = stpop2.toISOString();
+	var ediso = edpop2.toISOString();
+	insertEvent(summary,stpop2,edpop2,msgpop);//googleCalendarAPI.js
 	div_hide();
 }
 
